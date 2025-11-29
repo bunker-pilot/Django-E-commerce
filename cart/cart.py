f7579f041c44ff702510a66eccbdaf936bf2a543
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-11-29 19:39:49 +0330

    half baked cart

--- cart/cart.py
+++ cart/cart.py
@@ -5,7 +5,13 @@ class Cart:
         if not cart:
             cart = self.session["cart"] = {}
         self.cart = cart
-
+    def add(self, product , product_quantity):
+        product_id = str(product.id) 
+        if product_id in self.cart :
+            self.cart[product_id]["qtty"] += product_quantity
+        else:
+            self.cart[product_id] = {"price": product.price , "qtty" : product_quantity}
+        self.save()
     def save(self):
         self.session["cart"] = self.cart
         self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 # Create your views here.
 
 def cart_summary(request):
     return render(request , "cart/cart.html")
 
 def cart_add(request):
-    pass
-
+    cart = Cart(request)
+    if request.POST.get("action") == "POST" :
+        product_id = int(request.POST.get("product_id"))
+        product_quantity = int(request.POST.get("product_quantity"))
+        selected_product = get_object_or_404(Product, pk=product_id)
+        cart.add(product = selected_product, product_quantity=product_quantity)
+        response = JsonResponse({"product_name" : selected_product.title , "product_quantity" : product_quantity})
+        return response
 def cart_update(request):
     pass
 
