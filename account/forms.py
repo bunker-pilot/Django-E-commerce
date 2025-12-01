1bf98917965884611ceae6a7b2a4c008567894b3
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-12-01 13:55:46 +0330

    Account:initial setup

---
+++ account/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.forms import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField( required=True , max_length = 300)
+    class Meta:
+        model = User
+        fields = ["username" , "email" , "password1", "password2"]
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        if User.objects.filter(email = email).exists():
+            raise forms.ValidationError("Invalid Email")
+        
