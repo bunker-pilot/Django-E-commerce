203ae61906446e6b192322f764ef49c1156f67a1
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-12-01 15:26:27 +0330

    Account: register template

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.forms import forms
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class CreateUserForm(UserCreationForm):
@@ -11,6 +11,6 @@ class CreateUserForm(UserCreationForm):
     def clean_email(self):
         email = self.cleaned_data.get("email")
 
-        if User.objects.filter(email = email).exists():
-            raise forms.ValidationError("Invalid Email")
-        
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already in use")
+        return email
--- account/views.py
+++ account/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render 
-
+from django.shortcuts import render, get_object_or_404
+from .forms import CreateUserForm
+from django.views import View
+from django.urls import reverse_lazy
 # Create your views here.
+class RegisterView(View):
+    def get(self, request):
+        form = CreateUserForm()
+        
+    def post(self,reuqest):
+        form = CreateUserForm(reuqest.POST)
+        if form.is_valid():
+            form.save()
+        return render(request , "account/register.html" , {"form":form})
 
 def register(request):
-    return render(request , "account/register.html")
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    return render(request , "account/register.html" , {"form":form})
