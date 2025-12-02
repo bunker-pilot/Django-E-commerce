b6d8a98bf0d8a3d5c32b28dfe0859e5619ae0d97
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-12-02 15:36:38 +0330

    Email Verification: initial setup

--- account/views.py
+++ account/views.py
@@ -5,20 +5,14 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 # Create your views here.
 
-class RegisterView(CreateView):
-    template_name= "account/register.html"
-    form_class = CreateUserForm
-    success_url = reverse_lazy("store-app:store")
-    #context_object_name= "form" // Automatically names it form  :)
-"""
 class RegisterView(View):
     def get(self, request):
         form = CreateUserForm()
-        return render(request , "account/register.html" , {"form":form})
+        return render(request , "account/registeration/register.html" , {"form":form})
         
     def post(self,reuqest):
         form = CreateUserForm(reuqest.POST)
         if form.is_valid():
             form.save()
-        return render(request , "account/register.html" , {"form":form})
-"""
+        return render(request , "account/registeration/register.html" , {"form":form})
+
