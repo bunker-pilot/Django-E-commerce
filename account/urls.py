1381c05fb6febcdcdedbf300660363ddffe4ec4a
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-12-07 23:08:37 +0330

    Account: Dashboard template style

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name ="account-app"
 urlpatterns = [
@@ -17,5 +18,11 @@ urlpatterns = [
     # Dashboard /Profile Management
     path("profile-management" , views.ProfileManagementView.as_view() , name = "profile-management"),
     # Dashboard / Delete acc
-    path("del-account" , views.delete_account , name = "delete-user")
+    path("del-account" , views.delete_account , name = "delete-user"),
+    #Password reset view
+    path("reset-password/" , auth_views.PasswordResetView.as_view(template_name = "account/password/password-reset.html") , name = "reset_password"),
+    
+    path("reset-password-sent" , auth_views.PasswordResetDoneView.as_view(template_name= "account/password/password-reset-sent.html") , name = "password_reset_done"),
+    path("reset/<uidb64>/<token>/" , auth_views.PasswordResetConfirmView.as_view(template_name = "account/password/password-reset-form.html") , name = "password_reset_confirm"),
+    path("pass-reset-complete" , auth_views.PasswordResetCompleteView.as_view(template_name = "account/password/password-reset-complete.html") , name = "password_reset_complete")
 ]
