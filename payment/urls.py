1005ee306843e0618a17d6ebb6a6eea06103a2b4
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-12-13 17:47:11 +0330

    Payment: Checkout template

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("checkout/", views.CheckoutView.as_view() , name = "payment_checkout"),
     path("success/" , views.success , name= "payment_success"),
     path("failed/" , views.failed , name = "payment_failed")
+    
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-
+from django.views.generic import View
+from .forms import ShippingAddressForm
+from .models import ShippingAddress
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
@@ -9,3 +12,31 @@ def success(request):
 
 def failed(request):
     return render(request , "payment/payment-failed.html")
+
+def checkout(request):
+    return render(request , "payment/checkout.html")
+
+class CheckoutView(LoginRequiredMixin,View):
+    login_url = "account-app:user-login"
+    def get_shipping(self, user):
+        try: 
+            shipping = ShippingAddress.objects.get(user = user)
+        
+        except ShippingAddress.DoesNotExist:
+            shipping = None
+        return shipping
+    def get(self , request):
+        shipping = self.get_shipping(request.user)
+        form = ShippingAddressForm(instance=shipping)
+        return render(request, "payment/checkout.html" , {"form" : form})
+
+    def post(self , request):
+        shipping = self.get_shipping(request.user)
+        form = ShippingAddressForm(request.POST,instance=shipping)
+        if form.is_valid():
+            wait = form.save(commit= False)
+
+            wait.user = request.user
+            wait.save()
+            return redirect()
+        return render(request , "payment/checkout.html")
