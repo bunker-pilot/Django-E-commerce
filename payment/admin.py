0fe3e3ebf435df00b673a015ab2530d72f6f41a1
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-12-13 20:28:41 +0330

    Payment: Added order, order item models

--- payment/admin.py
+++ payment/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
 # Register your models here.
 
 
 @admin.register(ShippingAddress)
 class ShippingAdmin(admin.ModelAdmin):
     list_filter = ["created_at" , "updated_at"]
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+from store.models import Product
 # Create your models here.
 
 class ShippingAddress(models.Model):
@@ -18,3 +20,29 @@ class ShippingAddress(models.Model):
     def __str__(self):
         return f"{self.full_name}-{self.city}"
     
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=300)
+
+    email = models.EmailField(max_length=255)
+    shipping_address = models.TextField(max_length=10000)
+    amount_paid = models.DecimalField(max_digits=8 , decimal_places=2 , validators=[MinValueValidator(0)])
+
+    date_ordered = models.DateTimeField(auto_now_add= True)
+    user = models.ForeignKey(User,  on_delete=models.SET_NULL , related_name="orders",blank=True,null=True)
+    
+    def __str__(self):
+        return f"Order-#{self.id}"
+
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(Order , on_delete=models.CASCADE , null=True)
+    product = models.ForeignKey(Product ,on_delete=models.SET_NULL , null=True)
+    product_name = models.CharField(max_length=300)
+    quantity = models.PositiveIntegerField(default =1)
+
+    price = models.DecimalField(decimal_places=2 , max_digits=8 , validators=[MinValueValidator(0)])
+
+
+    def __str__(self):
+        return f"Order Item-{self.product}-#{self.id}"
